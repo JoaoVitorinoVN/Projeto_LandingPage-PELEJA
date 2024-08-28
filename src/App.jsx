a1c583afb71030ee0b8b1b359ddca93e6392d4c1
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-08-28 11:04:27 -0300

    Integrando secao Autoridade

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Header from './components/header/Header.jsx'
 import './css/Styles.css'
 import Whatsapp from './components/whatsapp/Whatsapp.jsx'
 import SecaoFormulario from './components/secoes/secaoFormulario/SecaoFormulario.jsx'
+import Autoridade from './components/secoes/section_autoridade/Autoridade.jsx'
 
 
 
@@ -15,7 +16,8 @@ function App() {
     <>
       <Header />
       <SecaoChamada />
-      <SecaoProvaSocial />
+      <Autoridade />
+
       <SecaoFormulario />
       <Whatsapp />
     </>
