f5b3bb840f84b3d0825bc095d2fe920d2f3c15b6
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-09-09 18:06:13 -0300

    Retirando projeto PRODUTOS

--- PRODUTOS/src/components/whatsapp/Whatsapp.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { FloatingWhatsApp } from 'react-floating-whatsapp'
-
-export default function Whatsapp() {
-
-    return (
-        <FloatingWhatsApp phoneNumber='+5534999299543' accountName='Peleja - Lilian' chatMessage='Seja bem-vindo a nossa equipe de suporte. Informe a sua mensagem abaixo:' statusMessage='online' placeholder='Digite a sua mensagem' allowClickAway={true} allowEsc={true} />
-    )
-}
