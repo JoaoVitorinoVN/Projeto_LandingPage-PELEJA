da9ef7d0a5e536e73be7d100fd5a3026d0e6c565
Vini-IFTM <vinicius.raphael@estudante.iftm.edu.br>
2024-09-10 19:45:24 -0300

    Implementação JSON - Seção Segmentos

--- src/components/secoes/secaoSegmentos/Segmentos.jsx
+++ src/components/secoes/secaoSegmentos/Segmentos.jsx
@@ -1,16 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import styles from './Segmentos.module.css';
-
-// Importe as imagens diretamente
-import foodImg from '../../../assets/food.jpg';
-import petsImg from '../../../assets/pets.jpg';
-import supermarketImg from '../../../assets/supermarket.jpg';
 import AnimatedTitles from '../../animation/AnimatedTitles';
+import segmentosData from '../../../assets/segmentos.json';
 
 function Carrossel() {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    try {
+      setData(segmentosData);
+      setLoading(false);
+    } catch (error) {
+      console.error('Erro ao carregar o JSON:', error);
+      setError(error);
+      setLoading(false);
+    }
+  }, []);
+
   const settings = {
     dots: true,
     infinite: true,
@@ -38,32 +49,26 @@ function Carrossel() {
     ]
   };
 
-  const data = [
-    {
-      name: 'Alimentos',
-      img: foodImg
-    },
-    {
-      name: 'Pets',
-      img: petsImg
-    },
-    {
-      name: 'SuperMarket',
-      img: supermarketImg
-    }
-  ];
+  const titulos = {
+    titulo: 'Para pessoas exigentes: PELEJA!',
+    subtitulo: 'Venha conhecer nossos segmentos representados!'
+  };
 
-  const titulos = {titulo: 'Para pessoas exigentes: PELEJA!', subtitulo: 'Venha conhecer nossos segmentos representados!'}
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
 
   return (
     <section className={styles.sectionSegmentos} id='segmentos'>
-
-      <AnimatedTitles titulos={titulos}/>
-
+      <AnimatedTitles titulos={titulos} />
       <div className={styles.carouselContainer}>
         <Slider {...settings}>
-          {data.map((d) => (
-            <div key={d.name} className={styles.carouselItem}>
+          {data.map((d, index) => (
+            <div key={index} className={styles.carouselItem}>
               <div className={styles.carouselContent}>
                 <div className={styles.carouselImageContainer}>
                   <img src={d.img} alt={d.name} className={styles.carouselImage} />
